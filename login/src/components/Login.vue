93dd3191f27fc8ccf139189d8d1fa6f1e8a56d48
Thành <cothvbdnnn>
2021-07-27 17:18:58 +0700

    env

--- login/src/components/Login.vue
+++ login/src/components/Login.vue
@@ -1,20 +1,45 @@
 <template>
   <div>
-    <h1 class="mb-3">Login</h1>
-    <bizfly-form ref="formLogin">
-      <bizfly-input placeholder="Email" class="email" v-model="formLogin.email" />
-      <bizfly-input placeholder="Password" class="password my-3" v-model="formLogin.password" type="password" />
-      <bizfly-button :loading="loading" type="primary" @click="onSubmit">
+    <Header title="Login" class="mb-m"></Header>
+    <bf-form ref="formLogin">
+      <bf-input placeholder="Email" class="email" v-model="formLogin.email" />
+      <bf-input
+        placeholder="Password"
+        class="password my-s"
+        v-model="formLogin.password"
+        type="password"
+      />
+      <bf-button :loading="loading" type="primary" @click="onSubmit">
         Sign In
-      </bizfly-button>
-    </bizfly-form>
+      </bf-button>
+    </bf-form>
+    <bf-radio-group @change="changeLang" v-model="lang" class="mt-m">
+      <bf-radio :label="'vi'">vi</bf-radio>
+      <bf-radio :label="'en'">en</bf-radio>
+    </bf-radio-group>
+    <bf-pagination
+      class="mt-m"
+      :pageSize="10"
+      :total="50"
+      :pageSizes="[10, 20, 30, 40, 50, 100]"
+      :currentPage="1"
+      layout="total ,sizes, prev, pager, next"
+    />
+    <el-pagination
+      class="mt-m"
+      :pageSize="10"
+      :total="50"
+      :pageSizes="[10, 20, 30, 40, 50, 100]"
+      :currentPage="1"
+      layout="total ,sizes, prev, pager, next"
+    />
   </div>
 </template>
 
 <script>
 import { axios, AUTH_TOKEN } from "@/utils/axios";
+import { Header } from "@todo/style";
 import Cookies from "js-cookie";
-
 export default {
   data() {
     return {
@@ -23,27 +48,55 @@ export default {
         password: "",
       },
       loading: false,
+      lang: "vi",
     };
   },
+  components: {
+    Header,
+  },
+  beforeMount() {
+    if (Cookies.get("lang") === "vi") {
+      this.lang = "vi";
+    } else {
+      this.lang = "en";
+    }
+  },
   methods: {
+    changeLang() {
+      if (this.lang === "vi") {
+        Cookies.set("lang", "vi");
+        location.reload();
+        return;
+      }
+      Cookies.set("lang", "en");
+      location.reload();
+    },
     async onSubmit() {
-      this.loading = true
-      const signIn = await axios.post("auth/signin", this.formLogin).catch(() => { this.loading = false })
-      localStorage.setItem("userEmail", `${signIn.data.email}`)
-      Cookies.set(AUTH_TOKEN, signIn.data.auth_token, { expires: 999999 })
-      this.loading = false
+      this.loading = true;
+      const signIn = await axios
+        .post("auth/signin", this.formLogin)
+        .catch(() => {
+          this.loading = false;
+        });
+      localStorage.setItem("userEmail", `${signIn.data.email}`);
+      Cookies.set(AUTH_TOKEN, signIn.data.auth_token);
+      this.loading = false;
       this.$router.push("/list");
     },
   },
 };
 </script>
 
-<style scoped >
+<style scoped>
 form {
   max-width: 300px;
   width: 100%;
   margin: auto;
   display: flex;
   flex-flow: column;
+}
+.el-pagination {
+  max-width: 600px;
+  margin: auto;
 }
 </style>
--- login/src/main.js
+++ login/src/main.js
@@ -1,9 +1,28 @@
 import Vue from "vue";
 import singleSpaVue from "single-spa-vue";
 import App from "./App.vue";
+import Cookies from "js-cookie";
 import router from "./router";
 
-window["bizfly-ui"].default.install(Vue);
+const bizflyUI = window["bizfly-ui"].default;
+
+const { BfButton, BfForm, BfInput, BfPagination, BfRadio, BfRadioGroup } =
+  bizflyUI;
+Vue.component("BfButton", BfButton);
+Vue.component("BfForm", BfForm);
+Vue.component("BfInput", BfInput);
+Vue.component("BfPagination", BfPagination);
+Vue.component("BfRadio", BfRadio);
+Vue.component("BfRadioGroup", BfRadioGroup);
+
+console.log(bizflyUI);
+
+if (Cookies.get("lang") === "vi") {
+  bizflyUI.locale(bizflyUI.languages.vi);
+} else {
+  bizflyUI.locale(bizflyUI.languages.en);
+}
+
 Vue.config.productionTip = false;
 
 const vueLifecycles = singleSpaVue({
